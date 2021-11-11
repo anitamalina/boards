4f043e79a307678a4000fbadbfc475a73741bb35
Anitamalina <anit@itu.dk>
2021-11-11 10:00:13 +0100

    everything and menu

---
+++ src/screens/Articles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Articles = () => {
+    return (
+        <div className="article-screen">
+            {console.log("I am running")}
+            <p>Articles Page</p>
+        </div>
+    )
+}
+
+export default Articles
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <p>Dashboard Page</p>
+        </div>
+    )
+}
+
+export default Dashboard
